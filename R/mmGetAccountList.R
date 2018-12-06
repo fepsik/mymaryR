030398ee0e9a8c79db367a58f89f456d44bb3afd
Nikita Kulik <mr.sorok04@yandex.ru>
2018-12-07 01:38:50 +0300

    Create mmGetAccountList.R

---
+++ R/mmGetAccountList.R
@@ -0,0 +1,31 @@
+#'Get Accounts
+
+mmGetAccountList <- function(acc_id = NULL, mm_token = new_token) {
+acc_id <- as.character(acc_id)
+
+answer <- GET("https://app.mymarilyn.ru/api/accounts",
+              add_headers("X-API-Account" = acc_id, "X-API-Token" = mm_token))
+
+stop_for_status(answer)
+if (answer$status_code == 200) message("Alright!") else message("Not Today!")
+
+dataraw <- content(answer, "parsed", "application/json")
+
+data <- data.frame()
+for (i in 1:length(dataRaw$items)){
+ 
+  dataRaw$items[[i]][c('id','type','status','name')][sapply(dataRaw$items[[i]][c('id','type','status','name')], is.null)] <- NA
+  dataTemp <- data.frame(t(as.data.frame(unlist(dataRaw$items[[i]][c('id','name','type','status')],recursive = T))),row.names = NULL)
+  data <- rbind(data, dataTemp)
+}
+
+dataTotal <- data.frame(
+  Id = as.character(data$id),
+  Name = as.character(data$name),
+  Type = as.character(data$type),
+  Status = as.character(data$status)
+)
+
+return(dataTotal)
+
+}
