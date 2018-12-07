07033e34ee007a03e4199d94d00870275cd50a53
Nikita Kulik <mr.sorok04@yandex.ru>
2018-12-07 15:52:41 +0300

    Another One

---
+++ R/mmGetChannels.R
@@ -0,0 +1,22 @@
+#'@export
+mmGetChannels <- function(mm_token = new_token) {
+  
+  answer <- GET("https://app.mymarilyn.ru/api/channels",add_headers("X-API-Token" = mm_token))
+  stop_for_status(answer)
+  
+  if (answer$status_code == 200) message("Complete") else message("Wrong")
+  
+  dataRaw <- httr::content(answer, "parsed", "application/json")
+  
+  data <- data.frame()
+  for (i in 1:length(dataRaw$items)){
+    #Replace NULL to NA
+    dataRaw$items[[i]][sapply(dataRaw$items[[i]], is.null)] <- NA
+    dataTemp <- data.frame(t(as.data.frame(unlist(dataRaw$items[[i]],recursive = T))),row.names = NULL)
+    data <- rbind(data, dataTemp)
+  }
+  dataFin <- data.frame(channel_name = as.character(data$name),
+                       channel_id = as.character(data$id))
+  return(dataFin)
+}
+
